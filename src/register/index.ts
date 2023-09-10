9f9542cfefdebde9b789c996bf578e4f7ec821c8
cola <szmintel@gmail.com>
2023-09-10 21:18:26 +0800

    Revert "refactor(core): remove i18n to single branch"
    This reverts commit bdccb3e63cdaba6f372cf2c75dbb3759312d6964.

--- src/register/index.ts
+++ src/register/index.ts
@@ -10,7 +10,9 @@ import { install as installRouter } from '@/route'
 import { install as installStore } from '@/store'
 import installProperty from './registerProperty'
 import installDirective from './directives/index'
+import i18n from '@/locales'
 export default (app: App) => {
+  app.use(i18n)
   installRouter(app)
   installStore(app)
   installDirective(app)

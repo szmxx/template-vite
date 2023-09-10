31d75c92fcdf5a38073e6598d1ede80eb7acac2a
cola <szmintel@gmail.com>
2023-09-10 21:18:26 +0800

    Revert "refactor(core): remove i18n to single branch"
    This reverts commit bdccb3e63cdaba6f372cf2c75dbb3759312d6964.

---
+++ src/locales/index.ts
@@ -0,0 +1,39 @@
+/*
+ * @Author: cola
+ * @Date: 2023-09-10 15:39:10
+ * @LastEditors: cola
+ * @Description:
+ */
+import { createI18n } from 'vue-i18n'
+const files = import.meta.glob('./lang/*/index.ts')
+import { IModule } from './types'
+
+const currentLocale = localStorage.getItem('lang') || 'zh-CN'
+
+const i18n = createI18n({
+  globalInjection: true,
+  mode: 'composition',
+  allowComposition: true,
+  legacy: false,
+  fallbackLocale: 'zh-CN',
+  locale: '',
+})
+
+setLocale(currentLocale, true)
+
+export async function setLocale(locale: string, bool?: boolean) {
+  if (!i18n.global.availableLocales.includes(locale) || bool)
+    await loadAsyncLocale(locale)
+  document.querySelector('html')?.setAttribute('lang', locale)
+  // axios.defaults.headers.common['Accept-Language'] = locale
+  localStorage.setItem('lang', locale)
+  i18n.global.locale.value = locale
+}
+
+export async function loadAsyncLocale(locale: string) {
+  const module = files[`./lang/${locale}/index.ts`]
+  const messages = (await module()) as IModule
+  i18n.global.setLocaleMessage(locale, messages.default)
+}
+
+export default i18n

3fd70692fac9b308ef9c2abdbd83cf73caf7574e
cola <szmintel@gmail.com>
2023-09-12 01:47:49 +0800

    feat(core): add i18n

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,7 @@ declare module 'vue' {
   export interface GlobalComponents {
     DarkToggle: typeof import('./src/components/DarkToggle.vue')['default']
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']

ee3d2ecd441eda1897b4476b95a606f048d8a95c
cola <szmintel@gmail.com>
2023-09-12 02:08:13 +0800

    fix(core): pref vant view

--- src/register/registerStyle.ts
+++ src/register/registerStyle.ts
@@ -10,3 +10,4 @@ import 'uno.css'
 import '@/style/global.scss'
 // https://vant-ui.github.io/vant/#/en-US/quickstart#4.-style-of-function-components
 import 'vant/es/toast/style'
+import 'vant/es/dialog/style'
